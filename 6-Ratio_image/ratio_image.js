6720dec28938a013800b0bc36d4e82d93dba614f
JuanBarbosaMora <portelnom0308@gmail.com>
2025-03-08 16:23:43 -0500

    add node modules jimp and axios

---
+++ 6-Ratio_image/ratio_image.js
@@ -0,0 +1,12 @@
+/*
+ * Crea un programa que se encargue de calcular el aspect ratio de una
+ * imagen a partir de una url.
+ * - Url de ejemplo:
+ *   https://raw.githubusercontent.com/mouredevmouredev/master/mouredev_github_profile.png
+ * - Por ratio hacemos referencia por ejemplo a los "16:9" de una
+ *   imagen de 1920*1080px.
+*/
+
+// * Usamos jimp para leer el tamaño de las imagenes 
+// npm install jimp axios -> jimp = tamaño ; axios = descargar imagen desde url
+
